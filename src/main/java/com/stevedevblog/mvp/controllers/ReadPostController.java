8dee0b81c0245749b6d5f4b7ece68ed20c6e5596
Steve Jones <steve.jones763@gmail.com>
2021-10-29 17:59:34 +0100

    Added Markdown Renderer class to deal with converting to HTML and added CSS for tables.

--- src/main/java/com/stevedevblog/mvp/controllers/ReadPostController.java
+++ src/main/java/com/stevedevblog/mvp/controllers/ReadPostController.java
@@ -2,14 +2,12 @@ package com.stevedevblog.mvp.controllers;
 
 import com.stevedevblog.mvp.domain.PersistedBlogPost;
 import com.stevedevblog.mvp.service.BlogPostService;
+import com.stevedevblog.mvp.service.MarkdownRenderer;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.commonmark.node.*;
-import org.commonmark.parser.Parser;
-import org.commonmark.renderer.html.HtmlRenderer;
 
 import java.util.Optional;
 
@@ -19,9 +17,11 @@ public class ReadPostController {
 
     public static final String PATH = "/blog-post";
     private final BlogPostService blogPostService;
+    private final MarkdownRenderer markdownRenderer;
 
-    public ReadPostController(BlogPostService blogPostService) {
+    public ReadPostController(BlogPostService blogPostService, MarkdownRenderer markdownRenderer) {
         this.blogPostService = blogPostService;
+        this.markdownRenderer = markdownRenderer;
     }
 
     @GetMapping("{id}")
@@ -32,11 +32,9 @@ public class ReadPostController {
 
     private String returnPage(String id, Model model, PersistedBlogPost post) {
         String content = post.getPostContent();
-        Parser parser = Parser.builder().build();
-        Node document = parser.parse(content);
-        HtmlRenderer renderer = HtmlRenderer.builder().build();
-        String markdown = renderer.render(document);
+        String markdown = markdownRenderer.markdownToHTML(content);
         model.addAttribute("pageTitle", post.getTitle());
+        model.addAttribute("headerImageUrl", post.getHeaderImageUrl());
         model.addAttribute("post", post);
         model.addAttribute("markdown-content", markdown);
         return "read-post";
