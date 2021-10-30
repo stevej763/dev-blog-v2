c8f3ae105be01e571d734d07863ab665769881e1
steve <steve.jones763@gmail.com>
2021-10-30 17:51:54 +0100

    Form based authentication now fully working.

--- src/main/java/com/stevedevblog/mvp/controllers/admin/LoginController.java
+++ src/main/java/com/stevedevblog/mvp/controllers/admin/LoginController.java
@@ -5,15 +5,17 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@Controller
-@RequestMapping(AdminLoginController.PATH)
-public class AdminLoginController {
 
-    public static final String PATH = "/admin-login";
+@Controller
+@RequestMapping(LoginController.PATH)
+public class LoginController {
+
+    public static final String PATH = "/login";
+    public static final String PAGE_TITLE = "Login";
 
     @GetMapping
     public String getLoginPage(Model model) {
-        model.addAttribute("pageTitle", "Login");
+        model.addAttribute("pageTitle", PAGE_TITLE);
         return "admin/login";
     }
 
--- src/main/java/com/stevedevblog/mvp/security/SecurityConfigurationService.java
+++ src/main/java/com/stevedevblog/mvp/security/SecurityConfigurationService.java
@@ -3,6 +3,7 @@ package com.stevedevblog.mvp.security;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -11,6 +12,10 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+
+import static com.stevedevblog.mvp.security.UserPermission.POST_WRITE;
+import static com.stevedevblog.mvp.security.UserRole.ADMIN;
 
 @Configuration
 @EnableWebSecurity
@@ -26,13 +31,15 @@ public class SecurityConfigurationService extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
+                .csrf().disable()
                 .authorizeRequests()
-                .antMatchers("/", "error", "/css/*", "/js/*", "/icons/*", "/blog-post/*")
-                .permitAll()
+                .antMatchers("/", "/error", "/css/*", "/js/*", "/icons/*", "/blog-post/*").permitAll()
+                .antMatchers("/edit-post/*", "/new-post").hasRole(ADMIN.name())
                 .anyRequest()
                 .authenticated()
                 .and()
-                .httpBasic();
+                .formLogin()
+                .loginPage("/login").permitAll();
     }
 
     @Override
@@ -41,7 +48,7 @@ public class SecurityConfigurationService extends WebSecurityConfigurerAdapter {
         UserDetails adminUser = User.builder()
                 .username("steve")
                 .password(passwordEncoder.encode("password"))
-                .roles(UserRole.ADMIN.name())
+                .roles(ADMIN.name())
                 .build();
 
         return new InMemoryUserDetailsManager(adminUser);
