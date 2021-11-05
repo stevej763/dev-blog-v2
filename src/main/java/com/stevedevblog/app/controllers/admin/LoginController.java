f19e31765265dfde9418a1e00d5a4c9f17fb14c5
Steve Jones <steve.jones763@gmail.com>
2021-11-05 19:54:00 +0000

    Added css for add and edit options on admin page.

--- src/main/java/com/stevedevblog/app/controllers/admin/LoginController.java
+++ src/main/java/com/stevedevblog/app/controllers/admin/LoginController.java
@@ -3,12 +3,8 @@ package com.stevedevblog.app.controllers.admin;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-
-import java.util.Objects;
-import java.util.Optional;
 
 
 @Controller
